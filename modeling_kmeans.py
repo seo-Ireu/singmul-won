8a74e4b30fcecdc7e38de83a52d238f38734c74d
NewPlus <32642002+NewPlus@users.noreply.github.com>
2022-06-25 04:15:03 +0900

    예상 실제 데이터 모델링 완료(이용환)<0624>

--- modeling_kmeans.py
+++ modeling_kmeans.py
@@ -1,28 +1,39 @@
+from h11 import Data
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-X_water = np.random.randint(0, 1024, 100) # 습도 센서 값(0 ~ 1023) 100개
-Y_light = np.random.randint(180, 501, 100) # 조도 센서 값(180 ~ 500) 100개
+X_water = np.random.randint(0, 1024, 10) # 습도 센서 값(0 ~ 1023) 10개
+Y_light = np.random.randint(180, 501, 10) # 조도 센서 값(180 ~ 500) 10개
+X_water_like = np.random.randint(600, 700, 90) # 습도 센서 값(600 ~ 700) 90개
+Y_light_like = np.random.randint(200, 250, 90) # 조도 센서 값(200 ~ 250) 90개
 
-plt.scatter(X_water, Y_light, s=100, c='b') # 그래프에 표시(2차원, x축은 습도 y축은 조도)
+X = np.array([])
+X = np.append(X, X_water)
+X = np.append(X, X_water_like)
+
+Y = np.array([])
+Y = np.append(Y, Y_light)
+Y = np.append(Y, Y_light_like)
+
+plt.scatter(X, Y, s=100, c='b') # 그래프에 표시(2차원, x축은 습도 y축은 조도)
 plt.show() # 그래프 띄우기 1차(데이터 시각화)
 
-X = np.zeros((100, 2)) # 2차원 배열 원소 100개
+Dataset = np.zeros((100, 2)) # 2차원 배열 원소 100개
 for i in range(100):
-    X[i] = [X_water[i],Y_light[i]] # [[습도, 조도], ... ]
+    Dataset[i] = [X[i], Y[i]] # [[습도, 조도], ... ]
 
 # 데이터 매칭 확인
-table = pd.DataFrame(X) # Pandas 표 생성
+table = pd.DataFrame(Dataset) # Pandas 표 생성
 table.columns = ['습도','조도'] # 칼럼 변경
 print(table) # 데이터 매칭 표로 확인
 
 from sklearn.cluster import KMeans # KMeans Clustering 불러오기
 Kmean = KMeans(n_clusters=1) # cluster 1개로 KMeans
-Kmean.fit(X) # model fitting
+Kmean.fit(Dataset) # model fitting
 
-plt.scatter(X_water, Y_light, s =100, c='b') # 모델 이용된 데이터 포인트들 그래프에 표시
+plt.scatter(X, Y, s =100, c='b') # 모델 이용된 데이터 포인트들 그래프에 표시
 plt.scatter(Kmean.cluster_centers_[0][0],Kmean.cluster_centers_[0][1], s=200, c='g', marker='s') # 센서 값 중 적정 값 중앙에 표시(Means)
 plt.show() # 그래프 띄우기 2차(데이터 시각화)
 
