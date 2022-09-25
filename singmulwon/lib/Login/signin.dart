85249be74f38787d4ff89436a4b0b0d36bba3f13
AiBiC-Data <whrnjsgh12@naver.com>
2022-09-25 10:14:58 +0900

    update login

--- singmulwon/lib/Login/signin.dart
+++ singmulwon/lib/Login/signin.dart
@@ -18,6 +18,7 @@ class SignIn extends StatefulWidget {
 class _SignIn extends State<SignIn> {
   TextEditingController userid = TextEditingController();
   TextEditingController pw = TextEditingController();
+  ScrollController _scrollController;
 
   Future login(BuildContext cont) async {
     if (userid.text == "" || pw.text == "") {
@@ -50,136 +51,161 @@ class _SignIn extends State<SignIn> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(4.0),
-      padding: const EdgeInsets.only(top: 10, bottom: 24),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.grey[400]),
-        borderRadius: BorderRadius.all(Radius.circular(25.0)),
-      ),
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(left: 18.0, top: 10),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Login',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return SingleChildScrollView(
+      controller: _scrollController,
+      child: Container(
+        margin: const EdgeInsets.all(4.0),
+        padding: const EdgeInsets.only(top: 10, bottom: 24),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.grey[400]),
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 18.0, top: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(15.0),
-            padding: const EdgeInsets.all(13.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey[400]),
-              borderRadius: BorderRadius.all(Radius.circular(25.0)),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  width: 360,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        icon: Icon(
-                          Icons.perm_identity,
-                        ),
-                        labelText: 'Id',
-                        hintText: 'Type your Id'),
-                    //validation 추가 필요
-                    controller: userid,
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(13.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey[400]),
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              ),
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    width: 360,
+                    child: TextField(
+                      onTap: () {
+                        _scrollController.animateTo(120.0,
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.ease);
+                      },
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(
+                            Icons.perm_identity,
+                          ),
+                          labelText: 'Id',
+                          hintText: 'Type your Id'),
+                      //validation 추가 필요
+                      controller: userid,
+                    ),
                   ),
-                ),
-                Divider(),
-                SizedBox(
-                  width: 360,
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        icon: Icon(Icons.lock),
-                        labelText: 'Password',
-                        hintText: 'Type password'),
-                    //validation 추가 필요
-                    controller: pw,
+                  Divider(),
+                  SizedBox(
+                    width: 360,
+                    child: TextField(
+                      onTap: () {
+                        _scrollController.animateTo(120.0,
+                            duration: Duration(milliseconds: 500),
+                            curve: Curves.ease);
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          icon: Icon(Icons.lock),
+                          labelText: 'Password',
+                          hintText: 'Type password'),
+                      //validation 추가 필요
+                      controller: pw,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                      ),
+                      // ignore: sort_child_properties_last
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: <Widget>[
+                          Text(
+                            'Sign in',
+                            style: TextStyle(fontSize: 28),
+                          ),
+                        ],
+                      ),
+                      textColor: Colors.white,
+                      color: Colors.green[700],
+                      padding: EdgeInsets.all(10),
+                      onPressed: () {
+                        login(context);
+                      },
+                    ),
                   ),
                 ),
               ],
             ),
-          ),
-          Row(
-            children: <Widget>[
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(12.0),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(12.0),
+                      ),
+                      // ignore: sort_child_properties_last
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: <Widget>[
+                          Text(
+                            'Sign up',
+                            style: TextStyle(fontSize: 28),
+                          ),
+                        ],
+                      ),
+                      textColor: Colors.white,
+                      color: Colors.green[700],
+                      padding: EdgeInsets.all(10),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUp()),
+                        );
+                      },
                     ),
-                    // ignore: sort_child_properties_last
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Text(
-                          'Sign in',
-                          style: TextStyle(fontSize: 28),
-                        ),
-                      ],
-                    ),
-                    textColor: Colors.white,
-                    color: Colors.green[700],
-                    padding: EdgeInsets.all(10),
-                    onPressed: () {
-                      login(context);
-                    },
                   ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            children: <Widget>[
-              Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-                  child: RaisedButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(12.0),
-                    ),
-                    // ignore: sort_child_properties_last
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      // ignore: prefer_const_literals_to_create_immutables
-                      children: <Widget>[
-                        Text(
-                          'Sign up',
-                          style: TextStyle(fontSize: 28),
-                        ),
-                      ],
-                    ),
-                    textColor: Colors.white,
-                    color: Colors.green[700],
-                    padding: EdgeInsets.all(10),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUp()),
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
